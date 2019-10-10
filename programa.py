ed50e01f74b538645ea84fc47b7818f267a398ff
leonardoProjects <Leonardosanttos0077@gmail.com>
2019-10-10 11:37:16 -0300

    Melhorando ainda mais...

--- programa.py
+++ programa.py
@@ -26,9 +26,16 @@ class Filme(Programa):
         super().__init__(nome, ano)
         self.duracao = duracao
     
+    def __str__(self):
+        return f'Nome: {self.nome}\nAno: {self.ano}\nDuração: {self.duracao}\nLikes: {self.likes}'
+    
 class Serie(Programa):
     
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+        
+    def __str__(self):
+        return f'Nome: {self.nome}\nAno: {self.ano}\nTemporadas: {self.temporadas}\nLikes: {self.likes}'
+    
    
--- teste.py
+++ teste.py
@@ -3,18 +3,8 @@ from programa import *
 filme = Filme('Esquadrão suicida', 2016, 120)
 serie = Serie('The 100', 2018, 4)
 
+filmes_e_series = [filme, serie] 
 
-serie.dar_liker() 
-serie.nome = 'The flash' 
-
-print(f'Nome: {filme.nome}\n'
-      f'Ano: {filme.ano}\n'
-      f'Duração: {filme.duracao}\n'
-      f'Likes: {filme.likes}')
-      
-print('-='*20) 
-
-print(f'Nome: {serie.nome}\n'
-      f'Ano: {serie.ano}\n'
-      f'Temporadas: {serie.temporadas}\n'
-      f'Likes: {serie.likes}')
+for programa in filmes_e_series:
+    print('-=' *20)
+    print(programa) 
